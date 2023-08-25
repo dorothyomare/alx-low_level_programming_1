5527422d0d2f38520208161c3eef0d3d49cc1c18
dorothyomare <daughtykerubo@gmail.com>
2023-08-24 23:12:05 -0700

    /**  * _strncat - concatenate two strings  * using at most n bytes from src  * @dest: input value  * @src: input value  * @n: input value  * Return: dest  */
    char *_strncat(char *dest, char *src, int n)
    {
            int i;
            int j;
    
            i = 0;
            while (dest[i] != '\0')
            {
                    i++;
            }
            j = 0;
            while (j < n && src[j] != '\0')
            {
            dest[i] = src [j];
            i++;
            j++;
            }
            dest[i] = '\0';
            return (dest);

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,30 +1,31 @@
+#include "main.h"
+
 /**
  * _strncat - concatenate two strings
- * @dest: destination string
- * @src: source string
- * @n: maximum number of bytes to concatenate
- * Return: pointer to the resulting string
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-    int i, j;
+        int i;
+        int j;
 
-    i = 0;
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
-
-    j = 0;
-    while (j < n && src[j] != '\0')
-    {
-        dest[i] = src[j];
+        i = 0;
+        while (dest[i] != '\0')
+        {
+                i++;
+        }
+        j = 0;
+        while (j < n && src[j] != '\0')
+        {
+        dest[i] = src [j];
         i++;
         j++;
-    }
-    dest[i] = '\0';
-
-    return (dest);
+        }
+        dest[i] = '\0';
+        return (dest);
 }
-
